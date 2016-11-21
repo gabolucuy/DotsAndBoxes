6672249a3c71a3b8eb5cd60748ffced8d617e9ba
Jeyson <jeyson.mirabal@gmail.com>
2016-11-21 23:15:19 +0100

    Turnos para cada jugador

--- lib/jugador.rb
+++ lib/jugador.rb
@@ -1,11 +1,20 @@
 class Jugador
   attr_accessor :score
 
-  def initialize()
+  def initialize(nombre)
     @score = 0
+    @nombre = nombre
   end
 
   def casillaCompletada()
     @score=@score+1
+  end
+
+  def cambiar_nombre(nombre)
+    @nombre = nombre
+  end
+
+  def nombre_jugador
+    return @nombre
   end
 end
--- spec/turno_spec.rb
+++ spec/turno_spec.rb
@@ -1,6 +1,17 @@
 require 'turno'
 
 describe Turno do
- 
 
+  it "deberia devolver al jugador1 por el turno incial" do
+      turno = Turno.new
+      jugador = turno.de_quien_es_turno?
+      jugador.nombre_jugador.should=="jugador1"
+  end
+
+  it "deberia devolver al jugador2 por ser su siguiente turno" do
+    turno = Turno.new
+    primer_turno = turno.de_quien_es_turno?  #jugador1
+    siguiente_turno = turno.de_quien_es_turno? #jugador2
+    siguiente_turno.nombre_jugador.should=="jugador2"
+  end
 end
